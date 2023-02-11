ff2363bc3a1c255b49f6feb648c30ad3346ae874
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-02-10 21:39:09 -0500

    affects Light and Barrier blocks now

---
+++ src/main/java/com/telepathicgrunt/structurevoidtoggle/behaviors/ShapeInterface.java
@@ -0,0 +1,11 @@
+package com.telepathicgrunt.structurevoidtoggle.behaviors;
+
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public interface ShapeInterface {
+
+    void setShape(VoxelShape SHAPE);
+
+    VoxelShape getShape();
+}
+
