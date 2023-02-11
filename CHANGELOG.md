e6eb18ce9207d60e080a16c3e550028253f63d32
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-02-10 20:58:10 -0500

    fixed fabulous mode rendering
    https://github.com/TelepathicGrunt/StructureVoidToggle/issues/4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,42 +1,4 @@
-### **(V.4.1.4 Changes) (1.19.3 Minecraft)**
+### **(V.4.2.0 Changes) (1.19.3 Minecraft)**
 
-#### Misc:
-Fixed some code behind the scenes to try and make sure mod doesn't crash server if accidentally put on server.
-
-
-### **(V.4.1.3 Changes) (1.19.3 Minecraft)**
-
-#### Behavior:
-Improved full cube Forced Rendering looks to use solid cube drawings instead of lines
-
-
-### **(V.4.1.2 Changes) (1.19.3 Minecraft)**
-
-#### Behavior:
-Forced Rendering shows larger cube when Structure Void hitbox is set to large
-
-
-### **(V.4.1.1 Changes) (1.19.3 Minecraft)**
-
-#### Misc:
-Updated to 1.19.3
-
-
-### **(V.4.1.0 Changes) (1.19.2 Minecraft)**
-
-#### Misc:
-Updated to 1.19.2
-
-Added separate category in Keybind menu for just this mod's keybinds
-
-#### Behavior:
-Added option to force render Structure Void, Barrier, and Light outside of Structure Blocks by clicking the DELETE key.
-
-#### Lang:
-Cleaned up the lang translations and wordings for everything to be more clear.
-
-
-### **(V.4.0.0 Changes) (1.19 Minecraft)**
-
-#### Misc:
-Ported to 1.19
+#### Fabulous Mode:
+Fixed outlines not rendering when in Fabulous Mode.
--- src/main/java/com/telepathicgrunt/structurevoidtoggle/mixin/client/LevelRendererMixin.java
+++ src/main/java/com/telepathicgrunt/structurevoidtoggle/mixin/client/LevelRendererMixin.java
@@ -2,24 +2,21 @@ package com.telepathicgrunt.structurevoidtoggle.mixin.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.telepathicgrunt.structurevoidtoggle.behaviors.ToggleBehavior;
-import net.minecraft.client.Camera;
-import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.client.renderer.LightTexture;
+import net.minecraft.client.renderer.RenderType;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(LevelRenderer.class)
 public class LevelRendererMixin {
 
-    @Inject(method = "renderLevel(Lcom/mojang/blaze3d/vertex/PoseStack;FJZLnet/minecraft/client/Camera;Lnet/minecraft/client/renderer/GameRenderer;Lnet/minecraft/client/renderer/LightTexture;Lorg/joml/Matrix4f;)V",
-            at = @At(value = "TAIL"),
-            locals = LocalCapture.CAPTURE_FAILSOFT)
-    private void keyPress(PoseStack poseStack, float f, long l, boolean bl, Camera camera, GameRenderer gameRenderer, LightTexture lightTexture, Matrix4f matrix4f, CallbackInfo ci) {
-        ToggleBehavior.forceRenderInvisibleBlocks(poseStack, camera, (LevelRenderer)(Object)this);
+    @Inject(method = "renderChunkLayer(Lnet/minecraft/client/renderer/RenderType;Lcom/mojang/blaze3d/vertex/PoseStack;DDDLorg/joml/Matrix4f;)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderType;clearRenderState()V"))
+    private void structurevoidtoggle_renderHook(RenderType renderType, PoseStack poseStack, double d, double e, double f, Matrix4f matrix4f, CallbackInfo ci) {
+        ToggleBehavior.forceRenderInvisibleBlocks(renderType, poseStack, Minecraft.getInstance().gameRenderer.getMainCamera(), (LevelRenderer)(Object)this);
     }
 }
