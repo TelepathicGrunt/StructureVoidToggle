c4ec7500b635dce87085aa58053dcbd7d3c3643e
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-02-12 15:36:06 -0500

    fixed mixin

--- src/main/java/com/telepathicgrunt/structurevoidtoggle/mixin/client/LevelRendererMixin.java
+++ src/main/java/com/telepathicgrunt/structurevoidtoggle/mixin/client/LevelRendererMixin.java
@@ -6,7 +6,6 @@ import com.telepathicgrunt.structurevoidtoggle.behaviors.ToggleBehavior;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.RenderType;
-import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(LevelRenderer.class)
 public class LevelRendererMixin {
 
-    @Inject(method = "renderChunkLayer(Lnet/minecraft/client/renderer/RenderType;Lcom/mojang/blaze3d/vertex/PoseStack;DDDLorg/joml/Matrix4f;)V",
+    @Inject(method = "renderChunkLayer(Lnet/minecraft/client/renderer/RenderType;Lcom/mojang/blaze3d/vertex/PoseStack;DDDLcom/mojang/math/Matrix4f;)V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderType;clearRenderState()V"))
     private void structurevoidtoggle_renderHook(RenderType renderType, PoseStack poseStack, double d, double e, double f, Matrix4f matrix4f, CallbackInfo ci) {
         ToggleBehavior.forceRenderInvisibleBlocks(renderType, poseStack, Minecraft.getInstance().gameRenderer.getMainCamera(), (LevelRenderer)(Object)this);
