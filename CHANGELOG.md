12460191a9dd52f16032643593d85f21e7e6815b
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-05-25 12:13:19 -0400

    fixed server crash

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## **(V.4.2.4 Changes) (1.19.4 Minecraft)**
+
+##### Misc:
+Fixed crash on server. This mod won't do anything on servers properly now.
+
+
 ## **(V.4.2.3 Changes) (1.19.4 Minecraft)**
 
 ##### Major:
--- src/main/java/com/telepathicgrunt/structurevoidtoggle/StructureVoidToggleClient.java
+++ src/main/java/com/telepathicgrunt/structurevoidtoggle/StructureVoidToggleClient.java
@@ -1,9 +1,11 @@
 package com.telepathicgrunt.structurevoidtoggle;
 
+import com.telepathicgrunt.structurevoidtoggle.behaviors.ToggleBehavior;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 
 import static com.telepathicgrunt.structurevoidtoggle.behaviors.ToggleBehavior.KEY_BIND_STRUCTURE_VOID_FORCED_RENDER_TOGGLE;
 import static com.telepathicgrunt.structurevoidtoggle.behaviors.ToggleBehavior.KEY_BIND_STRUCTURE_VOID_RENDER_TOGGLE;
@@ -14,6 +16,11 @@ public class StructureVoidToggleClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        WorldRenderEvents.BEFORE_BLOCK_OUTLINE.register((worldRenderContext, result) -> {
+            ToggleBehavior.forceRenderInvisibleBlocks(worldRenderContext.matrixStack(), worldRenderContext.camera(), worldRenderContext.worldRenderer());
+            return true;
+        });
+
         KeyBindingHelper.registerKeyBinding(KEY_BIND_STRUCTURE_VOID_TOGGLE);
         KeyBindingHelper.registerKeyBinding(KEY_BIND_STRUCTURE_VOID_RENDER_TOGGLE);
         KeyBindingHelper.registerKeyBinding(KEY_BIND_STRUCTURE_VOID_FORCED_RENDER_TOGGLE);
