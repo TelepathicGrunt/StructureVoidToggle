24ec09a304283ccab9c1d1b7af32cc7a236a9d54
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-08-28 16:37:03 -0400

    fixed large distance rendering

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## **(V.4.2.6 Changes) (1.20.1 Minecraft)**
+
+##### Rendering:
+Fixed outline/box drawing going nuts at large distances from world origin
+
+
 ## **(V.4.2.4 Changes) (1.20.1 Minecraft)**
 
 ##### Misc:
--- src/main/java/com/telepathicgrunt/structurevoidtoggle/behaviors/ToggleBehavior.java
+++ src/main/java/com/telepathicgrunt/structurevoidtoggle/behaviors/ToggleBehavior.java
@@ -27,6 +27,7 @@ import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.client.event.RenderLevelStageEvent;
 import org.joml.Matrix4f;
+import org.joml.Vector4d;
 import org.joml.Vector4f;
 import org.lwjgl.glfw.GLFW;
 
@@ -197,17 +198,17 @@ public class ToggleBehavior {
 			Player player = Minecraft.getInstance().player;
 			Level level = player.level();
 
-			float drawRadius;
+			double drawRadius;
 			if (MODE == STRUCTURE_BLOCK_MODE.FULL_HITBOX) {
-				drawRadius = 0.4f;
+				drawRadius = 0.4D;
 			}
 			else {
-				drawRadius = 0.05f;
+				drawRadius = 0.05D;
 			}
-			float minCorner = 0.5f - drawRadius;
-			float maxCorner = 0.5f + drawRadius;
-			Vector4f vector4fMin = new Vector4f(minCorner, minCorner, minCorner, 1.0F);
-			Vector4f vector4fMax = new Vector4f(maxCorner, maxCorner, maxCorner, 1.0F);
+			double minCorner = 0.5D - drawRadius;
+			double maxCorner = 0.5D + drawRadius;
+			Vector4d vector4dMin = new Vector4d(minCorner, minCorner, minCorner, 1.0D);
+			Vector4d vector4dMax = new Vector4d(maxCorner, maxCorner, maxCorner, 1.0D);
 
 			int radius = 40;
 			Vec3 cameraPos = event.getCamera().getPosition();
@@ -217,7 +218,6 @@ public class ToggleBehavior {
 
 			PoseStack poseStack = event.getPoseStack();
 			poseStack.pushPose();
-			poseStack.translate(-cameraPos.x, -cameraPos.y, -cameraPos.z);
 
 			Tesselator tesselator = Tesselator.getInstance();
 			RenderSystem.setShader(GameRenderer::getPositionColorShader);
@@ -299,12 +299,12 @@ public class ToggleBehavior {
 								renderQuadBox(
 										bufferbuilder,
 										poseStack.last().pose(),
-										vector4fMin.x() + worldSpot.getX(),
-										vector4fMin.y() + worldSpot.getY(),
-										vector4fMin.z() + worldSpot.getZ(),
-										vector4fMax.x() + worldSpot.getX(),
-										vector4fMax.y() + worldSpot.getY(),
-										vector4fMax.z() + worldSpot.getZ(),
+										(float) (vector4dMin.x() + worldSpot.getX() - cameraPos.x()),
+										(float) (vector4dMin.y() + worldSpot.getY() - cameraPos.y()),
+										(float) (vector4dMin.z() + worldSpot.getZ() - cameraPos.z()),
+										(float) (vector4dMax.x() + worldSpot.getX() - cameraPos.x()),
+										(float) (vector4dMax.y() + worldSpot.getY() - cameraPos.y()),
+										(float) (vector4dMax.z() + worldSpot.getZ() - cameraPos.z()),
 										(int) (red * distanceMult),
 										(int) (green * distanceMult),
 										(int) (blue * distanceMult),
@@ -314,12 +314,12 @@ public class ToggleBehavior {
 								renderLineBox(
 										bufferbuilder,
 										poseStack.last().pose(),
-										vector4fMin.x() + worldSpot.getX(),
-										vector4fMin.y() + worldSpot.getY(),
-										vector4fMin.z() + worldSpot.getZ(),
-										vector4fMax.x() + worldSpot.getX(),
-										vector4fMax.y() + worldSpot.getY(),
-										vector4fMax.z() + worldSpot.getZ(),
+										(float) (vector4dMin.x() + worldSpot.getX() - cameraPos.x()),
+										(float) (vector4dMin.y() + worldSpot.getY() - cameraPos.y()),
+										(float) (vector4dMin.z() + worldSpot.getZ() - cameraPos.z()),
+										(float) (vector4dMax.x() + worldSpot.getX() - cameraPos.x()),
+										(float) (vector4dMax.y() + worldSpot.getY() - cameraPos.y()),
+										(float) (vector4dMax.z() + worldSpot.getZ() - cameraPos.z()),
 										red,
 										green,
 										blue,
