19dfcf084d1fdc25edade07660fbe90fd4681886
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-06-13 21:31:19 -0400

    port to 1.21

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## **(V.4.3.1 Changes) (1.20.6 Minecraft)**
+## **(V.4.4.0 Changes) (1.21.0 Minecraft)**
 
 ##### Major:
-Updated to 1.20.6 MC
+Updated to 1.21.0 MC
--- src/main/java/com/telepathicgrunt/structurevoidtoggle/StructureVoidToggleClient.java
+++ src/main/java/com/telepathicgrunt/structurevoidtoggle/StructureVoidToggleClient.java
@@ -1,15 +1,20 @@
 package com.telepathicgrunt.structurevoidtoggle;
 
 import com.telepathicgrunt.structurevoidtoggle.behaviors.ToggleBehavior;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 import net.neoforged.neoforge.common.NeoForge;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+@Mod(value = StructureVoidToggleClient.MODID, dist = Dist.CLIENT)
 public class StructureVoidToggleClient {
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static final String MODID = "structure_void_toggle";
 
-    public static void onInitializeClient() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+    public StructureVoidToggleClient(IEventBus modEventBus) {
         modEventBus.addListener(StructureVoidToggleClient::registerKeyBinding);
         NeoForge.EVENT_BUS.addListener(ToggleBehavior::forceRenderInvisibleBlocks);
     }
